c566a54ae94e5ac70803505ecaa7a5408bfc72c6
devchaudhary102 <devc37440@gmail.com>
2025-11-04 14:55:39 +0530

    Medium

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> tails = new ArrayList<>();
+        for (int num : nums) {
+            int i = Collections.binarySearch(tails, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            if (i == tails.size()) {
+                tails.add(num);
+            } else {
+                tails.set(i, num);
+            }
+        }
+        return tails.size();
+    }
+}
