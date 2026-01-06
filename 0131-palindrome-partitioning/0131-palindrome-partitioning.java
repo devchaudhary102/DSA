ba910946815acbf6a09e11db47a46063b2add15b
Dev Chaudhary <devc37440@gmail.com>
2026-01-06 12:53:15 +0530

    0131-palindrome-partitioning 01-06-2026 Medium

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<String>> ans;
+    private boolean isPalindrome(int i,int j,String s){
+        while(i<j){
+            if(s.charAt(i++)!=s.charAt(j--)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private void helper(int idx,String s,List<String> al){
+        if(idx>=s.length()){
+            ans.add(new ArrayList(al));
+            return;
+        }
+        for(int i=idx;i<s.length();i++){
+            if(isPalindrome(idx,i,s)){
+                al.add(s.substring(idx,i+1));
+                helper(i+1,s,al);
+                al.remove(al.size()-1);
+            }
+        }
+
+    }
+    public List<List<String>> partition(String s) {
+        ans=new ArrayList<>();
+        helper(0,s,new ArrayList<>());
+        return ans;
+    }
+
+}
