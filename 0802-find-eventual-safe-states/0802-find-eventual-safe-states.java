6728dfa9402fbc5e7b66a4fb46dc7d8056987d4f
Dev Chaudhary <devc37440@gmail.com>
2026-04-16 11:38:41 +0530

    0802-find-eventual-safe-states 04-16-2026 Medium

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,37 +1,34 @@
 class Solution {
-    private boolean dfs(int node,int[][] graph,int vis[],int pvis[],int check[]){
-        vis[node]=1;
-        pvis[node]=1;
-        check[node]=0;
-        for(int it:graph[node]){
-            if(vis[it]==0){
-                if(dfs(it,graph,vis,pvis,check)==true) return true;
-            }
-            else if(pvis[it]==1){
-                return true;
-            }
-        }
-        check[node]=1;
-        pvis[node]=0;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        int vis[]=new int[n];
-        int pvis[]=new int[n];
-        int check[]=new int[n];
+        int n=graph.length;
+        List<List<Integer>> adjr=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(i,graph,vis,pvis,check);
+            adjr.add(new ArrayList<>());
+        }
+        int[] ind=new int[n];
+        for(int i=0;i<n;i++){
+            for(int it:graph[i]){
+                adjr.get(it).add(i);
+                ind[i]++;
             }
         }
-        List<Integer> al=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> sf=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(check[i]==1){
-                al.add(i);
+            if(ind[i]==0){
+                q.add(i);
             }
         }
-        return al;
-
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            sf.add(node);
+            for(int it:adjr.get(node)){
+                ind[it]--;
+                if(ind[it]==0) q.add(it);
+            }
+        }
+        Collections.sort(sf);
+        return sf;
     }
 }
